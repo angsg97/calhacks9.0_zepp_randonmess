2a3ad6597ac7577f63a86358db353801536560d3
angsg97 <songgee_ang@mymail.sutd.edu.sg>
2022-10-16 11:12:57 -0700

    Add sparkle animation to all screen

--- page/8ball.js
+++ page/8ball.js
@@ -21,6 +21,31 @@ Page({
     //   y: 100,
     //   text: `${diceRoll}`,
     // })
+
+    // Sparkle Animation
+    const imgAnimation = hmUI.createWidget(hmUI.widget.IMG_ANIM, {
+      anim_path: 'starz',
+      anim_prefix: 'starz',
+      anim_ext: 'png',
+      anim_fps: 30,
+      anim_size: 16,
+      repeat_count: 0,
+      anim_status: 3,
+      x: 0,
+      y: 0,
+      anim_complete_call: () => {
+        console.log('animation complete')
+      }
+    })
+
+    imgAnimation.setProperty(hmUI.prop.ANIM_STATUS, hmUI.anim_status.START)
+    imgAnimation.addEventListener(hmUI.event.CLICK_DOWN, () => {
+      const isRunning = imgAnimation.getProperty(hmUI.prop.ANIM_IS_RUNINNG)
+
+      if (!isRunning) {
+        imgAnimation.setProperty(hmUI.prop.ANIM_STATUS, hmUI.anim_status.START)
+      }
+    })
     
     hmUI.createWidget(hmUI.widget.TEXT, {
       ...EIGHT_BALL_TEXT,
--- page/index.js
+++ page/index.js
@@ -10,8 +10,35 @@ import {
   SPORTS_BUTTON,
 } from '../utils/styles'
 
+const music = hmSensor.createSensor(hmSensor.id.MUSIC)
+
 Page({
   build() {
+    // Sparkle Animation
+    const imgAnimation = hmUI.createWidget(hmUI.widget.IMG_ANIM, {
+      anim_path: 'starz',
+      anim_prefix: 'starz',
+      anim_ext: 'png',
+      anim_fps: 30,
+      anim_size: 16,
+      repeat_count: 0,
+      anim_status: 3,
+      x: 0,
+      y: 0,
+      anim_complete_call: () => {
+        console.log('animation complete')
+      }
+    })
+
+    imgAnimation.setProperty(hmUI.prop.ANIM_STATUS, hmUI.anim_status.START)
+    imgAnimation.addEventListener(hmUI.event.CLICK_DOWN, () => {
+      const isRunning = imgAnimation.getProperty(hmUI.prop.ANIM_IS_RUNINNG)
+
+      if (!isRunning) {
+        imgAnimation.setProperty(hmUI.prop.ANIM_STATUS, hmUI.anim_status.START)
+      }
+    })
+
     // Title
     hmUI.createWidget(hmUI.widget.TEXT, {
       ...TITLE_TEXT,
